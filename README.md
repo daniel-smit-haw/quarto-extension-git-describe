5c30f2b3c599981dc9e712bdeafe4adcbcfbd2ab
Daniel Smit (HAW) <daniel.smit@haw-hamburg.de>
2023-04-11 10:09:08 +0000

    Add initial version

---
+++ README.md
@@ -0,0 +1,25 @@
+# Git-describe Extension For Quarto
+
+This shortcode runs [`git describe`](https://git-scm.com/docs/git-describe) and returns the output. 
+
+## Installing
+
+```bash
+quarto add daniel-smit-haw/quarto-extension-git-describe
+```
+
+This will install the extension under the `_extensions` subdirectory.
+If you're using version control, you will want to check in this directory.
+
+`git` itself has to be installed.
+
+## Using
+
+Add the shortcode `{{{< git-describe >}}}` to your quarto files.
+
+Note that `git describe` itself will only produce an output if a suitable ref is available.
+
+## Example
+
+Here is the source code for a minimal example: [example.qmd](example.qmd).
+
---
+++ _extensions/git-describe/git-describe.lua
@@ -0,0 +1,27 @@
+-- DPS: Modified example from https://quarto.org/docs/extensions/shortcodes.html#named-arguments
+
+-- run git and read its output
+function git(command)
+  local p = io.popen("git " .. command)
+  if p ~= nil then
+    local output = p:read('*all')
+    p:close()
+    return output
+  else
+    return pandoc.Null
+  end
+end
+
+-- return a table containing shortcode definitions
+-- defining shortcodes this way allows us to create helper 
+-- functions that are not themselves considered shortcodes 
+return {
+  ["git-describe"] = function(args, kwargs)
+    -- run the command
+    local cmd = "describe"
+    local rev = git(cmd)
+    
+    -- return as string
+    return pandoc.Str(rev)
+  end
+}
